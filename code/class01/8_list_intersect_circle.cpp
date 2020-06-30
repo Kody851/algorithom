82f5284f0f55a92cdad06b7a64870414fada8b01
kody <2270493988@qq.com>
2020-07-01 00:01:34 +0800

    add

--- code/class01/8_list_intersect_circle.cpp
+++ code/class01/8_list_intersect_circle.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<unordered_set>
 using namespace std;
 /*===链表集大成====
 给定两个可能有环也可能无环的单链表， 头节点head1和head2。 请实
@@ -47,12 +48,32 @@ public:
 			n2 = n2->next->next;
 			n1 = n1->next;
 		}
+		//法1，简单，但是难理解
+		/*
 		n2 = head; // n2 -> walk again from head
 		while (n1 != n2) {
 			n1 = n1->next;
 			n2 = n2->next;
 		}
 		return n1;
+		*/
+		//法2
+		//计算环内结点数
+        int NumOfCurcle=1;
+        ListNode*cur=n1->next;
+        while(cur!=n1){
+            NumOfCurcle++;
+            cur=cur->next;
+        }
+        //定位环的入口结点
+        n1=head;
+        n2=head;
+        for(int i=0;i<NumOfCurcle;i++) n1=n1->next;
+        while(n1!=n2){
+            n1=n1->next;
+            n2=n2->next;
+        }
+        return n1;
 	}
 
 	// 如果两个链表都无环，返回第一个相交节点，如果不想交，返回null
@@ -71,7 +92,7 @@ public:
 			n--;
 			cur2 = cur2->next;
 		}
-		if (cur1 != cur2) {
+		if (cur1 != cur2) {//若有公共部分，则最后一个节点必相同。（Y字形）
 			return NULL;
 		}
 		// n  :  链表1长度减去链表2长度的值
@@ -88,16 +109,34 @@ public:
 		}
 		return cur1;
 	}
+	ListNode* FindFirstCommonNode( ListNode* pHead1, ListNode* pHead2) {//noLoop的hasn版本
+          if(pHead1==NULL||pHead2==NULL) return NULL;
+          unordered_set<ListNode*> hashset;
+          ListNode *pNode1=pHead1,*pNode2=pHead2;
+          //把链表2转存为哈希表
+          while(pNode2!=NULL){
+              hashset.insert(pNode2);
+              pNode2=pNode2->next;
+          }
+          //遍历第一个链表
+          while(pNode1!=NULL){
+            if(hashset.find(pNode1)!=hashset.end()){
+                return pNode1;
+            }
+            pNode1=pNode1->next;
+          }
+          return NULL;
+    }
 
 	// 两个有环链表，返回第一个相交节点，如果不想交返回null
 	ListNode* bothLoop(ListNode* head1, ListNode* loop1, ListNode* head2, ListNode* loop2) {
 		ListNode* cur1 = NULL;
 		ListNode* cur2 = NULL;
-		if (loop1 == loop2) {
+		if (loop1 == loop2) {//入环节点相同（Y+O）
 			cur1 = head1;
 			cur2 = head2;
 			int n = 0;
-			while (cur1 != loop1) {
+			while (cur1 != loop1) {//把入环节点当做Y的尾节点，用noLoop方法
 				n++;
 				cur1 = cur1->next;
 			}
@@ -117,15 +156,16 @@ public:
 				cur2 = cur2->next;
 			}
 			return cur1;
-		} else {
+		} 
+		else {//入环节点不同（-O-）
 			cur1 = loop1->next;
 			while (cur1 != loop1) {
 				if (cur1 == loop2) {
-					return loop1;
+					return loop1;//loop1与loop2两个答案都可以
 				}
 				cur1 = cur1->next;
 			}
-			return NULL;
+			return NULL;//-O -O不相交
 		}
 	}
 
