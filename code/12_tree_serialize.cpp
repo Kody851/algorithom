918a3f015fa1797be296f5846b37fb6ecee98afb
kody <2270493988@qq.com>
2020-07-16 23:46:16 +0800

    1

--- code/12_tree_serialize.cpp
+++ code/12_tree_serialize.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<queue>
 #include<stack>
+#include<regex>
 using namespace std;
 
 struct TreeNode {
@@ -11,6 +12,15 @@ struct TreeNode {
             val(x), left(NULL), right(NULL) {
     }
 };
+
+vector<string>split(string str, string sep){//自己写的split函数
+	//https://www.zhihu.com/question/36642771
+	std::regex split("\\"+sep);
+	vector<string>res(std::sregex_token_iterator(str.begin(),str.end(),split,-1),std::sregex_token_iterator());
+	//for(auto&& x:res)
+	//	cout<<x<<"\n";
+	return res;
+}
 class Solution{
 public:
     void preOrderRecur(TreeNode* head) {
@@ -30,22 +40,22 @@ public:
 		res += serialByPre(head->right);
 		return res;
 	}
+	
     TreeNode* reconByPreString(string preStr) {
-		vector<string> values = preStr.split("_");//https://www.zhihu.com/question/36642771
+		vector<string> values = split(preStr,"_");
 		queue<string> q;
 		for (int i = 0; i != values.size(); i++) {
 			q.push(values[i]);
 		}
 		return reconPreOrder(q);
 	}
-
 	TreeNode* reconPreOrder(queue<string> q) {
 		string value = q.front();
         q.pop();
-		if (value.equals("#")) {
+		if (value=="#") {
 			return NULL;
 		}
-		TreeNode* head = new TreeNode(Integer.valueOf(value));
+		TreeNode* head = new TreeNode(std::stoi(value));//std::stoi   string转int
 		head->left = reconPreOrder(q);
 		head->right = reconPreOrder(q);
 		return head;
@@ -61,8 +71,9 @@ int main(){
     Solution solu;
     //cout<<"pre-order: ";
     //solu.preOrderRecur(head);
-    cout<<"先序-序列化：";
-    cout<<solu.serialByPre(head);
-    
+    //cout<<"先序-序列化：";
+    //cout<<solu.serialByPre(head);
+	
+	
     return 0;
 }
