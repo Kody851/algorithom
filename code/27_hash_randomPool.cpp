e8314ab884234e84e4875d28d184f59602d1c472
kody <2270493988@qq.com>
2020-10-21 00:28:37 +0800

    1

---
+++ code/27_hash_randomPool.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include<unordered_set>
+#include<unordered_map>
+#include<map>
+
+//设计一种结构，在该结构中有如下三个功能:
+//insert(key):将某个key加入到该结构，做到不重复加入
+//delete(key):将原本在结构中的某个key移除
+//getRandom(): 等概率随机返回结构中的任何一个key。
+//【要求】Insert、delete和getRandom方法的时间复杂度都是O(1)
+
+class Solution{
+public:
+    
+};
