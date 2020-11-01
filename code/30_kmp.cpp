04fb837360d88d271a1c0bf0901f297d67eb647e
kody <2270493988@qq.com>
2020-11-01 22:48:52 +0800

    1

--- code/30_kmp.cpp
+++ code/30_kmp.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 //KMP算法
 //字符串匹配问题
+//字符串str1和str2，str1是否包含str2，如果包含返回str2在str1中开始的位置。不包含返回-1。 
+//如何做到时间复杂度O(N)完成？
 vector<int>getNextArray(string ms) {
 	if (ms.size() == 1) {
 		return vector<int>{-1};
@@ -16,8 +18,8 @@ vector<int>getNextArray(string ms) {
 	next[0] = -1;
 	next[1] = 0;
 	int i = 2; //数组的位置
-	int cn = 0;
-	while (i < next.size()) {
+	int cn = 0;//cn=next[i-1]
+	while (i < next.size()) {// O(N)
 		if (ms[i - 1] == ms[cn]) {
 			next[i++] = ++cn;
 		} 
@@ -25,12 +27,12 @@ vector<int>getNextArray(string ms) {
 			cn = next[cn];
 		} 
 		else {
-			next[i++] = 0;
+			next[i++] = 0;//跳转到开头也没找到ms[i - 1] == ms[cn]的情况，那么i位置的next取0
 		}
 	}
 	return next;
 }
-int getIndei1Of(string s, string m) {
+int getIndexOf(string s, string m) {
 	if (s.empty() || m.empty() || s.size() < m.size()) {
 		return -1;
 	}
@@ -40,6 +42,8 @@ int getIndei1Of(string s, string m) {
 	int i2 = 0;
 	vector<int>next = getNextArray(str2); // O (M)
 	// O(N)
+	//str1从i位置与str2的0位置开始配，当str1到x，str2到y时，不匹配了。
+	//这时，y回退到该元素的(最长前缀与最长后缀的)最大匹配长度坐标处，与x继续匹配
 	while  (i1 < str1.size() && i2 < str2.size()) {
 		if (str1[i1] == str2[i2]) {
 			i1++;
@@ -52,6 +56,7 @@ int getIndei1Of(string s, string m) {
 			i2 = next[i2];
 		}
 	}
+	//while循环停止意味着有字符串越界了
 	// i1 越界  或者  i2越界了
 	return i2 == str2.size() ? i1 - i2 : -1;
 }
@@ -60,6 +65,6 @@ int getIndei1Of(string s, string m) {
 int main(){
     string str = "abcabcababaccc";
 	string match = "ababa";
-	cout<<getIndei1Of(str, match);
+	cout<<getIndexOf(str, match);
     return 0;
 }
---
+++ code/31_slidingWindow.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+using namespace std;
+#include<unordered_set>
+#include<unordered_map>
+#include<map>
+#include<vector>
+#include<stack>
+/*
+【题目】 有一个整型数组arr和一个大小为w的窗口从数组的最左边滑到最右边，窗口每次向右边滑一个位置。 
+例如，数组为[4,3,5,4,3,3,6,7]，窗口大小为3时: 
+[4 3 5]4 3 3 6 7 
+4[3 5 4]3 3 6 7 
+4 3[5 4 3]3 6 7 
+4 3 5[4 3 3]6 7 
+4 3 5 4[3 3 6]7 
+4 3 5 4 3[3 6 7]  
+如果数组长度为n，窗口大小为w，则一共产生n-w+1个窗口的最大值。 
+请实现一个函数。 输入:整型数组arr，窗口大小为w。 
+输出:一个长度为n-w+1的数组res，res[i]表示每一种窗口状态下的 
+以本题为例，结果应该 返回{5,5,5,4,6,7}。
+*/
+void printVector(vector<int>arr){
+	int i=0;
+	for(;i<arr.size()-1;i++){
+		cout<<arr[i]<<",";
+	}
+	cout<<arr[i];
+}
+vector<int> maxInWindows(const vector<int>& num, unsigned int size)
+    {
+        vector<int>res;
+        if(num.empty()||size<=0||size>num.size()) return res;
+        deque<int>dq;//双端队列，存储的是index
+        for(unsigned int i=0;i<size;i++){
+            while(!dq.empty()&&num[i]>num[dq.back()])
+                dq.pop_back();
+            dq.push_back(i);
+        }
+        for(unsigned int i=size;i<num.size();i++){
+            res.push_back(num[dq.front()]);
+			//从队列后边操作，i处的值大于dq最后的值，就要从后边弹出
+            while(!dq.empty()&&num[i]>num[dq.back()])
+                dq.pop_back();
+			dq.push_back(i);
+			//最左边的数过期了，要去掉
+            if(!dq.empty()&&(i-dq.front()>=size))
+               dq.pop_front();   
+        }
+        res.push_back(num[dq.front()]);
+        return res;
+    }
+//更一般的，我们可以实现更广泛的情况：滑窗大小可以不固定，而是任意变化。
+//L和R都随意往右移动，只满足L不要跑到R右边即可。
+class WindowMax{
+public:
+	int L;
+	int R;
+	vector<int>arr;
+	deque<int>dq;
+	//R右移
+	void addNumFromRight() {
+		if(R == arr.size()) {
+			return;
+		}
+		while (!dq.empty() && arr[dq.back()] <= arr[R]) {
+			dq.pop_back();
+		}
+		dq.push_back(R);
+		R++;
+	}
+	//L右移
+	void removeNumFromLeft() {
+		if(L >= R-1) {
+			return;
+		}
+		L++;
+		if(dq.front() == L) {
+			dq.pop_front();
+		}
+	}
+	//任意时刻，当前的最大值
+	int getMax() {
+		if(!dq.empty()) {
+			return arr[dq.front()];
+		}
+		return -1;
+	}
+};
+////////////////////////
+int main(){
+	printVector(maxInWindows({ 4, 3, 5, 4, 3, 3, 6, 7 }, 3));
+    return 0;
+}
